ef4ba0344132c7838f68a43072874890b270afcc
ThienDuc <ntduc4@gmail.com>
2024-10-09 00:20:45 +0800

    Very basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Guess {
+  name: string;
+  difficutly: "PST" | "PRS" | "FTR" | "ETR" | "BYD";
 }
 
-export default App
+function App() {
+  const [guess, setGuess] = useState("");
+  const [diff, setDiff] = useState<Guess["difficutly"] | "">("");
+  const [pastGuesses, setPastGuesses] = useState<Guess[]>([]);
+  const [checkError, setCheckError] = useState(false);
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (diff === "") {
+            setCheckError(true);
+          } else {
+            setPastGuesses((prev) => [
+              ...prev,
+              { name: guess, difficutly: diff },
+            ]);
+            setDiff("");
+            setGuess("");
+          }
+        }}
+      >
+        <input
+          type="text"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value)}
+          placeholder="Song title"
+        />
+        <select
+          value={diff}
+          onChange={(e) => {
+            setDiff(e.target.value as Guess["difficutly"]);
+            setCheckError(false);
+          }}
+          className={`mt-1 block py-2 px-3 border ${
+            !checkError ? "border-gray-300" : "border-red-500"
+          } bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
+        >
+          <option value={"PST"}>Past</option>
+          <option value={"PRS"}>Present</option>
+          <option value={"FTR"}>Future</option>
+          <option value={"ETR"}>Eternal</option>
+          <option value={"BYD"}>Beyond</option>
+          <option value="" disabled hidden>
+            Difficulty
+          </option>
+        </select>
+        <button type="submit">Guess</button>
+      </form>
+
+      <div>
+        <div>{/** Current result here */}</div>
+        <div>
+          {pastGuesses.map((guess) => {
+            return <GuessCard guess={guess} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const GuessCard = ({ guess }: { guess: Guess }) => {
+  return (
+    <div>
+      {guess.name} - {guess.difficutly}
+    </div>
+  );
+};
+
+export default App;
